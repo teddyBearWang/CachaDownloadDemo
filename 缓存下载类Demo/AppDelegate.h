64ca06ad6c165e97bb5da6d6bcd3774fb9f38078
teddy <teddy@teddydeiMac.local>
2014-10-15 09:56:34 +0800

    Initial Commit

---
+++ 缓存下载类Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  缓存下载类Demo
+//
+//  Created by teddy on 14-10-15.
+//  Copyright (c) 2014年 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
